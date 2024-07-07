b8fa919a8691a7f5376efa348f78941a516aee1f
Ohm-Rajpal <137588636+Ohm-Rajpal@users.noreply.github.com>
2024-07-07 15:17:40 -0700

    Model is over fitting, but at least it's not failing completely lol

--- unet.py
+++ unet.py
@@ -1,51 +1,66 @@
 from keras import Input, Model
-from keras.layers import Conv2D, BatchNormalization, Activation, MaxPooling2D, Conv2DTranspose, Concatenate
+from keras.layers import Conv2D, BatchNormalization, Activation, MaxPooling2D, Conv2DTranspose, Concatenate, Dropout, \
+    concatenate
 
 
-def conv_block(inputs, num_filters):
-    x = Conv2D(num_filters, 3, padding='same')(inputs)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+# Credit goes to Digital Sreeni for U-net architecture
+def my_unet_model(n_classes=8, IMG_HEIGHT=128, IMG_WIDTH=128, IMG_CHANNELS=1):
+    # Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    s = inputs
 
-    x = Conv2D(num_filters, 3, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
+    # Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
 
-    return x
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
 
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
 
-def encoder_block(inputs, num_filters):
-    x = conv_block(inputs, num_filters)
-    p = MaxPooling2D((2, 2))(x)
-    return x, p
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
 
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
 
-def decoder_block(inputs, skip, num_filters):
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding='same')(inputs)
-    x = Concatenate()([x, skip])
-    return x
+    # Expansive path
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
 
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
 
-def unet_model(input_shape=(128, 128, 3), num_classes=8):
-    inputs = Input(input_shape)
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
 
-    # Encoder
-    s1, p1 = encoder_block(inputs, 64)
-    s2, p2 = encoder_block(p1, 128)
-    s3, p3 = encoder_block(p2, 256)
-    s4, p4 = encoder_block(p3, 512)
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
 
-    # Bottleneck
-    b1 = conv_block(p4, 1024)
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
 
-    # Decoder
-    d1 = decoder_block(b1, s4, 512)
-    d2 = decoder_block(d1, s3, 256)
-    d3 = decoder_block(d2, s2, 128)
-    d4 = decoder_block(d3, s1, 64)
+    model = Model(inputs=[inputs], outputs=[outputs])
 
-    # Output
-    outputs = Conv2D(num_classes, (1, 1), activation='softmax')(d4)  # Adjusted for num_classes
-
-    model = Model(inputs, outputs, name='UNET')
     return model
